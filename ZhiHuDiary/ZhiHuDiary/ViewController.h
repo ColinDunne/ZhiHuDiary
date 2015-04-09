9048adeed053e81d2f736705d966869be59f8ab2
QianChen <xgdjqc@126.com>
2015-04-09 14:14:16 +0800

    Initialization

---
+++ ZhiHuDiary/ZhiHuDiary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZhiHuDiary
+//
+//  Created by 钱辰 on 4/9/15.
+//  Copyright (c) 2015 qianchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
